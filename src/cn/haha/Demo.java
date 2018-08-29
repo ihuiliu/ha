b182b4b844ac96595ef1fb77b6f12a8efa8333a2
liuhui <469880837@qq.com>
2018-08-29 15:16:57 +0800

    Update Demo.java

--- src/cn/haha/Demo.java
+++ src/cn/haha/Demo.java
@@ -3,7 +3,7 @@ package cn.haha;
 public class Demo {
 
 	public static void main(String[] args) {
-
+		  System.out.println("8888888888888888888888888888888888888888888888888888888888888888888888888");
 	}
 
 }
